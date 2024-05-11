867b0efb08e24785e104b595b74c8e1e8dfecf5f
Ayush Kumar <kumarayushhj@gmail.com>
2024-05-11 16:36:55 +0530

    completed and uploaded

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Ayush</div>
+          
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Ayush</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Ayush</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
